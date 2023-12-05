4ba84cc05fd4176c6020d4d31f6977618546d503
blckpeppa <35289169+blckpeppa@users.noreply.github.com>
2023-12-05 17:28:45 +0300

    basic url template in polls/urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,1 +1,9 @@
 # map the view in views.py to a url in urls.py
+from django.urls import path
+
+from . import views
+
+# this contains the actual url patterns for polls app
+urlpatterns = [
+    path("" , views.index, name="index"),
+]
